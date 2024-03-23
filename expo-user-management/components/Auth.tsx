0ca356242731d86c9f26794369219140262155aa
James Daniel Cadavona <jamesdanielcadavona@Jamess-MacBook-Pro-3.local>
2024-03-22 20:54:04 -0700

    implementing fN & lN

--- expo-user-management/components/Auth.tsx
+++ expo-user-management/components/Auth.tsx
@@ -47,43 +47,78 @@ export default function Auth() {
 
   return (
     <View style={styles.container}>
-        <View style={styles.title_container}>
-            <Text style={styles.title}>Prayerwall</Text>
-            <Image source={require('../assets/logo.png')} style={styles.logo} />
-        </View>
-        <View style = {styles.form_container}>
-            <Input style ={styles.email_input}
-            label="Email"
-            leftIcon={{ type: 'font-awesome', name: 'envelope' }}
-            onChangeText={setEmail}
-            value={email}
-            placeholder="email@address.com"
-            autoCapitalize="none"
+      <View style={styles.title_container}>
+        <Text style={styles.title}>Prayerwall</Text>
+        <Image source={require('../assets/logo.png')} style={styles.logo} />
+      </View>
+      <View style={styles.form_container}>
+        {isSigningUp ? (
+          <>
+            <Input style={styles.input}
+              placeholder="First Name"
+              inputStyle={{ paddingLeft: 10}}
+              label="First Name"
+            
+              labelStyle={styles.label}
             />
-            <Input
-                label="Password"
-                leftIcon={{ type: 'font-awesome', name: 'lock' }}
-                onChangeText={setPassword}
-                value={password}
-                secureTextEntry={true}
-                placeholder="Password"
-                autoCapitalize="none"
+            <Input style={styles.input}
+              placeholder="Last Name"
+              inputStyle={{ paddingLeft: 10}}
+              label="Last Name"
+            
+              labelStyle={styles.label}
             />
-            {isSigningUp ? (
-                <>
-                <Button title="Sign Up" disabled={loading} onPress={signUpWithEmail} />
-                <Button title="Already have an account? Log In" type="clear" onPress={toggleForm} />
-                </>
-            ) : (
-                <>
-                <Button title="Log In" disabled={loading} onPress={signInWithEmail} />
-                <Button title="Need an account? Sign Up" type="clear" onPress={toggleForm} />
-                </>
-            )}
-        </View>
-      
+            <Input style={styles.input}
+              placeholder="email@address.com"
+              onChangeText={setEmail}
+              value={email}
+              inputStyle={{ paddingLeft: 10}}
+              label="Email"
+              autoCapitalize="none"
+              labelStyle={styles.label}
+            />
+            <Input style={styles.input}
+              placeholder="Password"
+              onChangeText={setPassword}
+              value={password}
+              secureTextEntry={true}
+              inputStyle={{ paddingLeft: 10}}
+              label="Password"
+              autoCapitalize="none"
+              labelStyle={styles.label}
+            />
+            <Button buttonStyle={styles.auth_btn} titleStyle={styles.auth_btn_text} title="Sign Up" disabled={loading} onPress={signUpWithEmail} />
+            <Button title="Already have an account? Log In" type="clear" onPress={toggleForm} />
+          </>
+        ) : (
+          <>
+            <Input style={styles.input}
+              placeholder="email@address.com"
+              onChangeText={setEmail}
+              value={email}
+              inputStyle={{ paddingLeft: 10}}
+              label="Email"
+              autoCapitalize="none"
+              labelStyle={styles.label}
+            />
+            <Input style={styles.input}
+              placeholder="Password"
+              onChangeText={setPassword}
+              value={password}
+              secureTextEntry={true}
+              inputStyle={{ paddingLeft: 10}}
+              label="Password"
+              autoCapitalize="none"
+              labelStyle={styles.label}
+            />
+            <Button buttonStyle={styles.auth_btn} titleStyle={styles.auth_btn_text} title="Log In" disabled={loading} onPress={signInWithEmail} />
+            <Button title="Need an account? Sign Up" type="clear" onPress={toggleForm} />
+          </>
+        )}
+      </View>
     </View>
   );
+  
 }
 
 const styles = StyleSheet.create({
@@ -111,9 +146,31 @@ const styles = StyleSheet.create({
   },
   form_container: {
     padding: 12,
+    alignItems: 'center',
   },
-  email_input: {
-
+  label: {
+    fontFamily: 'JosefinSans-SemiBoldItalic',
+    marginBottom: 8,
+    color: '#030303'
+  },
+  input: {
+    fontFamily: 'JosefinSans-SemiBoldItalic',
+    backgroundColor: '#ffffff',
+    width: 327,
+    height: 50,
+    borderRadius: 2,
+    lineHeight: 19,
+    color: '#a9a9a9',
+    paddingRight: 20
+  },
+  auth_btn:{
+    backgroundColor: '#3a506b',
+    color: '#ffffff', 
+    height: 50,
+    width: 327,
+  },
+  auth_btn_text: {
+    fontFamily: 'JosefinSans-SemiBoldItalic',
   },
   verticallySpaced: {
 
