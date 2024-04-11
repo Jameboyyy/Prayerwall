77fd32a13c8b676300d972dde327abd2bfd38a0c
James Daniel Cadavona <jdpcadavona@gmail.com>
2024-04-10 17:50:39 -0700

    insert User_Profiles

--- expo-user-management/App.tsx
+++ expo-user-management/App.tsx
@@ -1,56 +1,27 @@
 import 'react-native-url-polyfill/auto'
 import { useState, useEffect } from 'react'
 import { supabase } from './lib/supabase'
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Auth from './components/Auth'
 import Account from './components/Account'
 import { View } from 'react-native'
 import { Session } from '@supabase/supabase-js'
-import { useFonts } from 'expo-font'
-
-const Stack = createNativeStackNavigator();
 
 export default function App() {
-
-  let [fontsLoaded] =  useFonts({
-    'JosefinSans-Regular': require('./fonts/JosefinSans-Regular.ttf'),
-    'JosefinSans-SemiBoldItalic': require('./fonts/JosefinSans-SemiBoldItalic.ttf'),
-  })
-
-
   const [session, setSession] = useState<Session | null>(null)
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
       setSession(session)
-    });
+    })
 
-    const { data: authListener } = supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-    });
-
-    return () => {
-      authListener.unsubscribe();
-    };
-  }, []);
-
-  if (!fontsLoaded) {
-    return null; 
-  }
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        {session ? (
-          // If there is a session, navigate to the Account screen
-          <Stack.Screen name="Account" component={Account} options={{ title: 'Account Details' }} />
-        ) : (
-          // No session found, show the Auth screen
-          <Stack.Screen name="Auth" component={Auth} options={{ title: 'Sign Up / Log In' }} />
-        )}
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    <View>
+      {session && session.user ? <Account key={session.user.id} session={session} /> : <Auth />}
+    </View>
+  )
 }
--- expo-user-management/components/Account.tsx
+++ expo-user-management/components/Account.tsx
@@ -1,39 +1,138 @@
-import React, { useState } from 'react';
-import { View, StyleSheet, Button, TextInput } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, Alert } from 'react-native';
+import { Button, Input, Text, Image } from 'react-native-elements';
+import { supabase } from '../lib/supabase';
+import { Session } from '@supabase/supabase-js';
 
-function Account({ navigation }) {
+export default function Account({ session }: { session: Session }) {
+  const [loading, setLoading] = useState(true);
+  const [user_name, setUsername] = useState('');
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [dateOfBirth, setDateOfBirth] = useState('');
-  const [username, setUsername] = useState('');
+  
 
-  const handleSubmit = async () => {
-    // Placeholder for submitting the user details
-    // Here you would typically update the user's profile in your backend
-    console.log(firstName, lastName, dateOfBirth, username);
-
-    // Optionally navigate the user to another screen upon successful update
-    // navigation.navigate('AnotherScreen');
+  const completeProfile = async () => {
+    setLoading(true);
+    try {
+      // Fetching the current user's details
+      const { data: { user }, error: userError } = await supabase.auth.getUser();
+  
+      if (userError) throw userError;
+      if (!user) throw new Error('No user on the session!');
+  
+      // Inserting new profile details
+      const { data, error } = await supabase
+        .from('User_Profiles')
+        .insert([{
+          id: user.id, // Assuming 'id' is the column in 'User_Profiles' linked to the user's ID from auth
+          user_name: user_name,
+          firstName: firstName,
+          lastName: lastName,
+          dateOfBirth: dateOfBirth,
+        }])
+        .single(); // Use .single() if inserting a single row and you expect a single object response
+  
+      if (error) throw error;
+  
+      Alert.alert("Profile completion successful!");
+    } catch (error) {
+      console.error(error);
+      if(error instanceof Error){
+        Alert.alert('Error', error.message);
+      } else {
+        // Handling cases where the error might not be an instance of Error
+        Alert.alert('Error', 'An unexpected error occurred.');
+      }
+    } finally {
+      setLoading(false);
+    }
   };
+
 
   return (
     <View style={styles.container}>
-      <TextInput placeholder="First Name" value={firstName} onChangeText={setFirstName} />
-      <TextInput placeholder="Last Name" value={lastName} onChangeText={setLastName} />
-      <TextInput placeholder="Date of Birth" value={dateOfBirth} onChangeText={setDateOfBirth} />
-      <TextInput placeholder="Username" value={username} onChangeText={setUsername} />
-      <Button title="Submit" onPress={handleSubmit} />
+      <View style={styles.title_container}>
+        <Text style={styles.title}>Prayerwall</Text>
+        <Image source={require('../assets/logo.png')} style={styles.logo} />
+      </View>
+      {/* User's Username */}
+      <View style={styles.verticallySpaced}>
+        <Input
+          label="Username"
+          value={user_name}
+          onChangeText={(text) => setUsername(text)}
+        />
+      </View>
+      {/* User's First Name */}
+      <View style={styles.verticallySpaced}>
+        <Input
+          label="First Name"
+          value={firstName}
+          onChangeText={setFirstName}
+        />
+      </View>
+      {/* User's Last Name */}
+      <View style={styles.verticallySpaced}>
+        <Input
+          label="Last Name"
+          value={lastName}
+          onChangeText={setLastName}
+        />
+      </View>
+      {/* User's Date of Birth */}
+      <View style={styles.verticallySpaced}>
+        <Input
+          label="Date of Birth"
+          value={dateOfBirth}
+          onChangeText={setDateOfBirth}
+        />
+      </View>
+      {/* Update Profile Button */}
+      <View style={[styles.verticallySpaced, styles.mt20]}>
+        <Button
+          title={loading ? 'Loading ...' : 'Complete Profile'}
+          onPress={completeProfile}
+          disabled={loading}
+        />
+      </View>
+      {/* Sign Out Button */}
+      <View style={styles.verticallySpaced}>
+        <Button title="Sign Out" onPress={() => supabase.auth.signOut()} />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    padding: 20,
+    height: '100%',
+    backgroundColor: '#d2e7d6',
   },
-  // Add styles for your inputs and button as needed
+  title_container: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: '25%'
+  },
+  title:{
+    fontSize: 32,
+    color: '#3a506b',
+    fontFamily: 'JosefinSans-SemiBoldItalic',
+    fontWeight: '600',
+    lineHeight: 42,
+    textAlign: 'center',
+  },
+  logo: {
+    width: 40,
+    height: 100,
+  },
+  verticallySpaced: {
+    paddingTop: 4,
+    paddingBottom: 4,
+    alignSelf: 'stretch',
+  },
+  mt20: {
+    marginTop: 20,
+  },
 });
-
-export default Account;
